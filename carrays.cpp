dbe583b1c42fe8c01de167e0306f157cb5cefb03
Michael L. Collard, Ph.D <collard@uakron.edu>
2020-02-07 09:10:37 -0500

    Add c-array average() with size parameter

---
+++ carrays.cpp
@@ -0,0 +1,15 @@
+/*
+    carrays.cpp
+
+    Definitions of c-array parameter examples
+*/
+
+// average of elements in the first size elements of the array ar
+double average(int ar[], int size) {
+
+    int total = 0;
+    for (int i = 0; i < size; ++i)
+        total += ar[i];
+
+    return (double) total / size;
+}
---
+++ carrays.hpp
@@ -0,0 +1,13 @@
+/*
+    carrays.hpp
+
+    Declaration of c-array parameter examples
+*/
+
+#ifndef INCLUDE_CARRAYS_HPP
+#define INCLUDE_CARRAYS_HPP
+
+// average of elements in the first size elements of the array ar
+double average(int ar[], int size);
+
+#endif
